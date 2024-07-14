be1840946c6013c7e2e0248c81800444c9c218a5
kubota20 <doc0000doc77@gmail.com>
2024-07-14 16:30:22 +0900

    HomePageを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+import EventNews from "@/features/home/event-news";
+import ProductList from "@/features/home/product-list";
+import TopSlides from "@/features/home/top-slides";
+import Footer from "@/features/home/home-footer";
+
+const HomePage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      home
-    </main>
+    <div className="flex flex-col">
+      <TopSlides />
+      <ProductList />
+      <EventNews />
+      <Footer />
+    </div>
   );
-}
+};
+
+export default HomePage;
