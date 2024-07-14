7f7db6dc3703fc6ae4b2e475f32a949a1832f357
kubota20 <doc0000doc77@gmail.com>
2024-07-14 16:25:10 +0900

    テスト用データ

---
+++ src/datatest/product-data.ts
@@ -0,0 +1,51 @@
+import Product1 from "@/datatest/images/product1.png";
+import Product2 from "@/datatest/images/product2.png";
+import Product3 from "@/datatest/images/product3.jpg";
+import Product4 from "@/datatest/images/product4.png";
+import Product5 from "@/datatest/images/product5.jpg";
+
+export type ProductDataProps = {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+};
+
+export const ProductData = [
+  {
+    id: 1,
+    name: "教育",
+    image: Product1,
+    description: "簡単な勉強方法を覚えて、記憶力向上する。",
+    price: 1000,
+  },
+  {
+    id: 2,
+    name: "自然を守ろう",
+    image: Product2,
+    description: "昔々あるところに...",
+    price: 1500,
+  },
+  {
+    id: 3,
+    name: "人体の仕組み",
+    image: Product3,
+    description: "脳、心臓、内臓、血管、筋肉それ等の仕組みを学ぶ",
+    price: 1000,
+  },
+  {
+    id: 4,
+    name: "教育",
+    image: Product4,
+    description: "読書１０００冊で全てが変わる",
+    price: 1300,
+  },
+  {
+    id: 5,
+    name: "小説",
+    image: Product5,
+    description: "森の中の子供達の物語",
+    price: 1700,
+  },
+];
---
+++ src/datatest/top-data.ts
@@ -0,0 +1,31 @@
+import TopImage1 from "@/datatest/images/top1.jpg";
+import TopImage2 from "@/datatest/images/top2.jpg";
+import TopImage3 from "@/datatest/images/top3.jpg";
+
+export interface TopImageProps {
+  id: number;
+  title: string;
+  image: string;
+  buttonText: string;
+}
+
+export const TopData = [
+  {
+    id: 1,
+    title: "Books Site",
+    image: TopImage1,
+    buttonText: "商品を見に行く",
+  },
+  {
+    id: 2,
+    title: "Books Site",
+    image: TopImage2,
+    buttonText: "商品を見に行く",
+  },
+  {
+    id: 3,
+    title: "Books Site",
+    image: TopImage3,
+    buttonText: "商品を見に行く",
+  },
+];
