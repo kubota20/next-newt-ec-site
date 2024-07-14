24bec0c2c10905ddfe29dd4be1cb2cab2ff662e9
kubota20 <doc0000doc77@gmail.com>
2024-07-14 16:23:34 +0900

    RouteLayoutを作成

---
+++ src/app/(routes)/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/layouts/footer";
+
+export default function RouteLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      {children}
+      <Footer />
+    </>
+  );
+}
