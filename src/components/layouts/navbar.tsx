384c625a5538b3546eaab8d75ed83f1cc019449d
kubota20 <doc0000doc77@gmail.com>
2024-07-16 14:23:40 +0900

    スマホ用ナビゲーションバーを作成

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,21 +1,36 @@
-import Link from "next/link";
-import Container from "../elements/container";
+import React from "react";
+
+// icon
+import { Menu } from "lucide-react";
+
+// components
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/elements/sheet";
 import { MainNav } from "./main-nav";
 
-const Navbar = () => {
+export const Navbar = () => {
   return (
-    <header className="fixed z-10 w-full">
-      <Container>
-        <div className="flex justify-between items-center">
-          <Link href="/">
-            <h1 className="text-2xl font-bold">Books</h1>
-          </Link>
-
-          <MainNav />
-        </div>
-      </Container>
-    </header>
+    <div className="flex items-center ">
+      <Sheet>
+        <SheetTrigger>
+          <Menu />
+        </SheetTrigger>
+        <SheetContent className="bg-white" side="left">
+          <SheetHeader>
+            <SheetTitle>メニュー</SheetTitle>
+          </SheetHeader>
+          <div className="my-10">
+            <nav className="flex flex-col gap-2">
+              <MainNav />
+            </nav>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </div>
   );
 };
-
-export default Navbar;
