7b2ed34e90d7f544d432108c678fe46d5b90b29f
kubota20 <doc0000doc77@gmail.com>
2024-07-22 15:25:18 +0900

    SingleNewsPageを追加

---
+++ src/app/(routes)/news/[newsId]/page.tsx
@@ -0,0 +1,11 @@
+import { News } from "@/features/news/news";
+
+const SingleNewsPage = () => {
+  return (
+    <div className="flex flex-col">
+      <News />
+    </div>
+  );
+};
+
+export default SingleNewsPage;
