6c911ee0ee6c2786df52ac08158b4257b94f9889
kubota20 <doc0000doc77@gmail.com>
2024-07-22 15:25:06 +0900

    CategoryPageを追加

---
+++ src/app/(routes)/categories/[categoryId]/page.tsx
@@ -0,0 +1,12 @@
+// features
+import { Product } from "@/features/products/product";
+
+const CategoryPage = () => {
+  return (
+    <div className="flex flex-col ">
+      <Product />
+    </div>
+  );
+};
+
+export default CategoryPage;
