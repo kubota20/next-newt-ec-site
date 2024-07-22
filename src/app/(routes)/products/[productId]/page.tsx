45b7d634b166f554ebd9b2a9014f0c0667e1a47d
kubota20 <doc0000doc77@gmail.com>
2024-07-22 15:25:32 +0900

    ProductPageを追加

---
+++ src/app/(routes)/products/[productId]/page.tsx
@@ -0,0 +1,11 @@
+import { Product } from "@/features/products/product";
+
+const ProductPage = () => {
+  return (
+    <div className="flex flex-col ">
+      <Product />
+    </div>
+  );
+};
+
+export default ProductPage;
