96f5e67c86fb816ba314fa60eeae6376b44161ee
kubota20 <doc0000doc77@gmail.com>
2024-08-11 18:51:55 +0900

    useRouterを使って動的ページのリンクを作成

--- src/features/products/product-card.tsx
+++ src/features/products/product-card.tsx
@@ -1,19 +1,32 @@
+"use client";
+
 import Image from "next/image";
 
 // test
-import { ProductDataProps } from "@/datatest/product-data";
+// import { ProductDataProps } from "@/datatest/product-data";
+
+// type
+import { ProductProps } from "@/types/types";
 
 // components
 import { Button } from "@/components/elements/button";
 
 // icon
 import { ChevronRight, ShoppingCart } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
-  item: ProductDataProps;
+  item: ProductProps;
 }
 
 export const ProductCard: React.FC<ProductCardProps> = ({ item }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/products/${item._id}
+`);
+  };
+
   return (
     <>
       <div className="bg-gray-300  w-full h-32 lg:h-52  group rounded-full border space-y-4 relative overflow-hidden text-nowrap">
@@ -21,7 +34,7 @@ export const ProductCard: React.FC<ProductCardProps> = ({ item }) => {
           {/* 画像 */}
           <Image
             src={item.image}
-            alt={item.name}
+            alt={item.title}
             className="object-cover rounded-full w-full h-32 lg:h-52 "
           />
 
@@ -31,16 +44,20 @@ export const ProductCard: React.FC<ProductCardProps> = ({ item }) => {
               <Button size="icon" className="size-7 md:size-8 lg:size-10">
                 <ShoppingCart size={20} className="text-gray-600" />
               </Button>
-              <Button size="icon" className="size-7 md:size-8 lg:size-10">
+              <Button
+                size="icon"
+                className="size-7 md:size-8 lg:size-10"
+                onClick={handleClick}
+              >
                 <ChevronRight size={20} className="text-gray-600" />
               </Button>
             </div>
           </div>
 
           {/* 名前 値段 */}
-          <div className="absolute z-10 top-0 flex max-sm:flex-col items-center justify-around bg-black/35 rounded-lg w-full py-2 px-4 text-white">
-            <h3 className="text-xs md:text-base">{item.name}</h3>
-            <p className="text-sm md:text-xl md:font-black">{item.price}円</p>
+          <div className="absolute z-10 top-0 flex flex-col items-center justify-around bg-black/35 rounded-lg w-full py-2 px-4 text-white">
+            <h3 className="text-xs md:text-xl">{item.title}</h3>
+            <p className="text-sm md:text-base md:font-black">{item.price}円</p>
           </div>
         </div>
       </div>
