65e01d4f1ca3ddae34965d889c20574af62a2a7f
kubota20 <doc0000doc77@gmail.com>
2024-08-11 18:38:04 +0900

    newt sdk を作成

---
+++ src/lib/newt.ts
@@ -0,0 +1,15 @@
+import "server-only";
+import { createClient } from "newt-client-js";
+
+export const NewtClient = createClient({
+  spaceUid: process.env.NEWT_SPACE_UID + "",
+  token: process.env.NEWT_CDN_API_TOKEN + "",
+  apiType: "cdn",
+});
+
+//　Newt SDK のメソッド
+
+// getContents()  コンテンツ一覧を取得する
+// getContent() 個別のコンテンツを取得する
+// getFirstContent() 最初のコンテンツを取得する
+// getApp() アプリを取得する
