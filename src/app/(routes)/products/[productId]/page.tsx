4995a2e42c0dbe5a16494babc96dd22fb05e74d5
kubota20 <doc0000doc77@gmail.com>
2024-08-11 18:49:02 +0900

    テスト用ProductDataをコメントアウト、getProductとgetProductsメソッドを追加しfindメソッドでgetProductsのidとgetProductのidを一致させ動的ルートが完成

--- src/app/(routes)/products/[productId]/page.tsx
+++ src/app/(routes)/products/[productId]/page.tsx
@@ -1,9 +1,23 @@
-import { Product } from "@/features/products/product";
+import { getProducts } from "@/actions/get-products";
 
-const ProductPage = () => {
+import { Product } from "@/features/products/product";
+import { getProduct } from "@/actions/get-product";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+const ProductPage = async ({ params }: Props) => {
+  const articles = await getProducts();
+  const data = await getProduct(params.productId);
+
+  const ProductData = articles.find((item) => item._id === data._id);
+
   return (
     <div className="flex flex-col ">
-      <Product />
+      <Product item={ProductData} />
     </div>
   );
 };
