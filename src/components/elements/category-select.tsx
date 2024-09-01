e3e10bdf6d1e982fdbf9d92665246fac31f06ded
kubota20 <doc0000doc77@gmail.com>
2024-09-01 20:02:10 +0900

    Selectにカテゴリ追加

--- src/components/elements/category-select.tsx
+++ src/components/elements/category-select.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+"use client";
 import {
   Select,
   SelectContent,
@@ -8,21 +8,48 @@ import {
   SelectValue,
 } from "@/components/elements/select";
 
-export const CategorySelect = () => {
+// types
+import { CategoryProps } from "@/types/types";
+import { useRouter, useParams } from "next/navigation";
+
+type CategorySelectProps = {
+  catItem: CategoryProps[] | undefined;
+};
+
+export const CategorySelect = ({ catItem }: CategorySelectProps) => {
+  const router = useRouter();
+
+  const params = useParams();
+
+  if (!catItem) {
+    return <div>中身なし</div>;
+  }
+
+  const handleChange = (value: string) => {
+    // 選択されたカテゴリのslugに基づいてページ遷移
+    router.push(`/categories/${value}`);
+  };
+
+  const currentCategory = catItem.find((cat) => cat._id === params.category);
+
   return (
     <div>
-      <Select>
-        <SelectTrigger className="w-[180px] bg-white">
-          <SelectValue placeholder="カテゴリー選択" />
+      <Select onValueChange={handleChange}>
+        <SelectTrigger className="min-w-[180px] bg-white ">
+          <SelectValue
+            placeholder={
+              // カテゴリページ遷移したらplaceholderの表示が選択したカテゴリ名になる
+              // それ以外は"カテゴリ選択"と表示
+              currentCategory ? currentCategory.name : "カテゴリ選択"
+            }
+          />
         </SelectTrigger>
         <SelectContent>
-          <SelectGroup>
-            <SelectItem value="教育">教育</SelectItem>
-            <SelectItem value="文学">文学</SelectItem>
-            <SelectItem value="ノンフィクション">ノンフィクション</SelectItem>
-            <SelectItem value="ビジネス">ビジネス</SelectItem>
-            <SelectItem value="歴史">歴史</SelectItem>
-          </SelectGroup>
+          {catItem.map((item) => (
+            <SelectGroup key={item._id}>
+              <SelectItem value={item._id}>{item.name}</SelectItem>
+            </SelectGroup>
+          ))}
         </SelectContent>
       </Select>
     </div>
