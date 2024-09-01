b45289e3619d09afb5c33e0097030ad4f5a5c2a5
kubota20 <doc0000doc77@gmail.com>
2024-09-01 20:03:35 +0900

    Command追加、タイトル検索機能を追加

--- src/components/elements/title-search.tsx
+++ src/components/elements/title-search.tsx
@@ -1,3 +1,10 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+import { useParams, useRouter } from "next/navigation";
+
 // components
 import {
   Popover,
@@ -17,24 +24,61 @@ import { Button } from "@/components/elements/button";
 // icon
 import { Search } from "lucide-react";
 
-export const TitleSearch = () => {
+// type
+import { ProductProps } from "@/types/types";
+
+type Props = {
+  productData: ProductProps[];
+};
+
+export const TitleSearch = ({ productData }: Props) => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  const params = useParams();
+  const router = useRouter();
+
+  const fromattedItems = productData.map((item) => ({
+    value: item._id,
+    label: item.title,
+  }));
+
+  const handleSelect = (item: { value: string; label: string }) => {
+    router.push(`/products/${item.value}`);
+    setOpen(false);
+  };
+
   return (
     <div>
-      <Popover>
+      <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
-          <Button size="icon" className="border">
+          <Button
+            size="icon"
+            className="border"
+            role="combobox"
+            aria-expanded={open}
+          >
             <Search />
           </Button>
         </PopoverTrigger>
         <PopoverContent className="bg-white">
-          <Command>
+          <Command
+            value={value}
+            onValueChange={(newValue) => setValue(newValue)}
+          >
             <CommandInput placeholder="タイトル検索" />
             <CommandList>
               <CommandEmpty>見つかりませんでした</CommandEmpty>
-              <CommandGroup heading="Suggestions">
-                <CommandItem>Calendar</CommandItem>
-                <CommandItem>Search Emoji</CommandItem>
-                <CommandItem>Calculator</CommandItem>
+              <CommandGroup>
+                {fromattedItems.map((item, index) => (
+                  <CommandItem
+                    key={item.label}
+                    value={item.label}
+                    onSelect={() => handleSelect(item)}
+                  >
+                    {index < 5 && item.label}
+                  </CommandItem>
+                ))}
               </CommandGroup>
             </CommandList>
           </Command>
