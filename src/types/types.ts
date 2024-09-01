b298738920db7924f4ee1286c884c1a89a2887ed
kubota20 <doc0000doc77@gmail.com>
2024-09-01 20:04:09 +0900

    typeを追加

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,22 +1,21 @@
-export interface ProductProps {
-  _id: string;
-  createdAt: string;
+import type { Content } from "newt-client-js";
+
+export type ProductProps = {
   title: string;
   image: ImageProps;
   description: string;
   price: number;
   category: CategoryProps[];
-}
+} & Content;
 
-export interface ImageProps {
-  _id: string;
+export type ImageProps = {
   src: string;
   title: string;
   height: number;
   width: number;
-}
+} & Content;
 
-export interface CategoryProps {
-  _id: string;
+export type CategoryProps = {
   name: string;
-}
+  slug: string;
+} & Content;
