e8ffb7a4786d463b43cfe9c54bd7c1f5b0d41886
kubota20 <doc0000doc77@gmail.com>
2024-09-26 15:10:25 +0900

    CartAction作成

---
+++ src/components/layouts/cart-action.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import CartButton from "@/components/ui/cart-button";
+import { useCart } from "@/hooks/use-cart";
+
+const CartAction = () => {
+  const [showCartButton, setShowCartButton] = useState(false);
+  const cart = useCart();
+
+  useEffect(() => {
+    if (cart.items.length > 0) {
+      // カートに商品が追加されたらボタンを表示
+      setShowCartButton(true);
+
+      // 5秒後にボタンを非表示にする
+      const timer = setTimeout(() => {
+        setShowCartButton(false);
+      }, 5000);
+
+      // クリーンアップ関数でタイマーをクリア
+      return () => clearTimeout(timer);
+    }
+  }, [cart.items.length]); // カートのアイテム数が変更されたときに実行
+
+  return (
+    <>
+      {showCartButton && (
+        <div className="fixed z-50 bottom-6 right-6">
+          <CartButton className="bg-black rounded-full" />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CartAction;
