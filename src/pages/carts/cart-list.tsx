63da6a989f4a9e4d4823e198082257e093eee1c8
kubota20 <doc0000doc77@gmail.com>
2024-09-26 15:13:03 +0900

    useCartを入れてカート機能を作成

--- src/pages/carts/cart-list.tsx
+++ src/pages/carts/cart-list.tsx
@@ -1,16 +1,49 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+// hooks
+import { useCart } from "@/hooks/use-cart";
+
 // features
 import { CartCard } from "@/pages/carts/cart-card";
+import { Button } from "@/components/ui/button";
 
 // test
-import { ProductData } from "@/datatest/product-data";
+// import { ProductData } from "@/datatest/product-data";
 
 export const CartList = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const cart = useCart();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  const handleClearCart = () => {
+    cart.removeAll();
+  };
   return (
     <div>
+      <h2 className="text-center mb-10 font-bold text-3xl">
+        カート({cart.items.length})
+      </h2>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-        {ProductData.map((item) => (
-          <CartCard key={item.id} item={item} />
+        {cart.items.map((item) => (
+          <CartCard key={item._id} item={item} />
         ))}
+      </div>
+
+      <div className="mt-6 text-center">
+        {cart.items.length > 0 && (
+          <Button variant="destructive" onClick={handleClearCart}>
+            カートを全て削除
+          </Button>
+        )}
       </div>
     </div>
   );
