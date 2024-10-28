673bde52585108364a7f01f1cbeb0469aa07de95
kubota20 <doc0000doc77@gmail.com>
2024-10-28 19:30:21 +0900

    テスト中

---
+++ src/lib/stripe.ts
@@ -0,0 +1,13 @@
+// Stripe 決済システム
+import Stripe from "stripe";
+
+export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+  apiVersion: "2024-09-30.acacia",
+  typescript: true,
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
